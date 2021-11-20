f855e89f81d963800773f5c01ecdcbb72bc3cfa9
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-20 19:16:10 +0700

    feat: use preact for production

--- next.config.js
+++ next.config.js
@@ -9,4 +9,17 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 };
