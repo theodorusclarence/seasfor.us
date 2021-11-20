fcb28da26d007c691cc5f92b680c93f74bc06b47
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-20 11:53:28 +0700

    feat: add events page barebone

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,5 +57,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
