7d82ebacf0e4ee797583c79f301770f08b6f9b96
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-21 15:50:03 +0700

    feat: simulate participants

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,16 +4,21 @@ import { EventsApi } from '@/types/api';
 
 export const parseEventsData = (data?: EventsApi): Product[] => {
   return (
-    data?.data.map((d) => ({
-      id: d.id,
-      name: d.name,
-      description: d.description,
-      href: `/event/${d.id}`,
-      participants: d.participant + '',
-      date: new Date(d.date),
-      city: d.city.name,
-      imageSrc: d.link_image,
-      imageAlt: `Picture of ${d.name}`,
-    })) ?? []
+    data?.data.map((d) => {
+      const date = new Date(d.date);
+      const day = date.getDate();
+
+      return {
+        id: d.id,
+        name: d.name,
+        description: d.description,
+        href: `/event/${d.id}`,
+        participants: day * 9 + d.participant + '',
+        date: new Date(d.date),
+        city: d.city.name,
+        imageSrc: d.link_image,
+        imageAlt: `Picture of ${d.name}`,
+      };
+    }) ?? []
   );
 };
