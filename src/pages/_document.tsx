908ce80dd597f1aa71e51fed5d39d1f741cd071d
rizqitsani <rizqitsani@gmail.com>
2021-11-21 20:56:23 +0700

    feat: add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,13 @@ class MyDocument extends Document {
             type='font/woff2'
             crossOrigin='anonymous'
           />
+          <script
+            async
+            defer
+            data-website-id='ac9b0cfe-d0a4-4e6c-9672-be94c66c025b'
+            src='https://umami.thcl.dev/umami.js'
+            data-domains='seasfor.us'
+          />
         </Head>
         <body>
           <Main />
